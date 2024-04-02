f45b8bab9d61051169d3d2011b9ae7f09a80e632
drop0ne <core_fire@hotmail.com>
2024-04-01 19:54:42 -0500

    refactored.  c2360 init of 'game' is skipped

--- Corefire24/Corefire24.cpp
+++ Corefire24/Corefire24.cpp
@@ -1,8 +1,7 @@
 // Corefire24.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include "Class0ne.h"
-
-void run_NumberGuessingGame();
+#include "NumberGuessingGame/NumberGuessingGame.h"
 
 int main()
 {
@@ -17,7 +16,7 @@ int main()
 
 		switch (fn.selectMenuOption())
 		{
-		case 1: run_NumberGuessingGame(); break;
+		case 1: auto game = new Game; game->run(); break;
 		case 2: fn.cout("Option 2 was slected\n"); break;
 		case 3: fn.cout("Option 3 was slected\n"); break;
 		case 9: fn.setMainLoopCondition(false); break;
@@ -28,48 +27,3 @@ int main()
 
 	return 0;
 }
-
-void run_NumberGuessingGame() {
-    const int rng_minimum{ 0 };// The new minimum value for the random number range
-    const int rng_maximum{ 100 };// The new maximum value for the random number range
-    const int MAX_GUESSES{ 10 };// Set the guess limit
-    int attempt{ 0 };
-    int attemptCount{ 0 };
-    bool winner = false;
-
-    // Get the handle to the console output
-    HANDLE console_HWND = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    auto numberGenerator = NumberGenerator(rng_minimum, rng_maximum);
-    int randomNumber = numberGenerator.getRNG();
-
-    // ******* ////// Game Loop \\\\\\ *******
-
-    do {
-        std::cout << "\nGuess a number between 0 and 100";
-        if (std::cin >> attempt) {
-            ++attemptCount;
-        }
-
-        std::cout << "You have " << (MAX_GUESSES - attemptCount) << " attempts remaining.\n";
-
-        if (attempt == randomNumber) {
-            winner = true;
-            break;
-        }
-        //Escape code to end game
-        if (attempt < 0) {
-            break;
-        }
-        if (attempt < randomNumber) {
-            std::cout << "No! My number is bigger!\n";
-        }
-        else {
-            std::cout << "No.  My number is smaller\n";
-        }
-        // If the player ran out of guesses, stop the game
-        if (attempt >= MAX_GUESSES) {
-            break;
-        }
-    } while (!winner);
-}// end of game // return to main //
--- Corefire24/NumberGuessingGame/NumberGuessingGame.h
+++ Corefire24/NumberGuessingGame/NumberGuessingGame.h
@@ -9,49 +9,16 @@ private:
     const int MAX_GUESSES{ 10 };// Set the guess limit
     int attempt{ 0 };
     int attemptCount{ 0 };
+    int randomNumber{ 0 };
     bool winner = false;
-
-    // Get the handle to the console output
 
 public:
 	Game();
 	~Game();
-
+    void run();
 private:
-    void initializeRNG() {
-        auto numberGenerator = NumberGenerator(rng_minimum, rng_maximum);
-        int randomNumber = numberGenerator.getRNG();
-    }
-
-    void gameLoop() {
-        do {
-            std::cout << "\nGuess a number between 0 and 100";
-            if (std::cin >> attempt) {
-                ++attemptCount;
-            }
-
-            std::cout << "You have " << (MAX_GUESSES - attemptCount) << " attempts remaining.\n";
-
-            if (attempt == randomNumber) {
-                winner = true;
-                break;
-            }
-            //Escape code to end game
-            if (attempt < 0) {
-                break;
-            }
-            if (attempt < randomNumber) {
-                std::cout << "No! My number is bigger!\n";
-            }
-            else {
-                std::cout << "No.  My number is smaller\n";
-            }
-            // If the player ran out of guesses, stop the game
-            if (attempt >= MAX_GUESSES) {
-                break;
-            }
-        } while (!winner);
-    }
+    void initializeRNG();
+    void gameLoop();
 };
 
 Game::Game()
@@ -60,4 +27,44 @@ Game::Game()
 
 Game::~Game()
 {
+}
+
+void Game::run() {
+    initializeRNG();
+    gameLoop();
+}
+
+void Game::initializeRNG() {
+    auto numberGenerator = NumberGenerator(rng_minimum, rng_maximum);
+    this->randomNumber = numberGenerator.getRNG();
+}
+
+void Game::gameLoop() {
+    do {
+        std::cout << "\nGuess a number between 0 and 100";
+        if (std::cin >> attempt) {
+            ++attemptCount;
+        }
+
+        std::cout << "You have " << (MAX_GUESSES - attemptCount) << " attempts remaining.\n";
+
+        if (attempt == randomNumber) {
+            winner = true;
+            break;
+        }
+        //Escape code to end game
+        if (attempt < 0) {
+            break;
+        }
+        if (attempt < randomNumber) {
+            std::cout << "No! My number is bigger!\n";
+        }
+        else {
+            std::cout << "No.  My number is smaller\n";
+        }
+        // If the player ran out of guesses, stop the game
+        if (attempt >= MAX_GUESSES) {
+            break;
+        }
+    } while (!winner);
 }
