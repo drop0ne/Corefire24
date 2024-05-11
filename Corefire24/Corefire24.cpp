6539613a5f68888aca1cf49cdddbbf38d1cb9b2b
Jake <102178397+drop0ne@users.noreply.github.com>
2024-05-11 00:42:39 -0500

    formating the color of the text printed to screen

--- Corefire24/Corefire24.cpp
+++ Corefire24/Corefire24.cpp
@@ -6,20 +6,18 @@
 int main()
 {
 	MyConsoleAPI_extended fn;
-
-	// Number Guessing Game START
-	NumberGenerator RNG;
 	NumberGuessingGame numberGuessingGame;
-	// END ****************
-	// More programs
 	CannaCalculator cannaCalcultor;
 	Quize quize;
 
  	do {
 		fn.clearScreen();
-		fn.cout("CoreFireCode 2024 edition\n", green);
-		fn.cout("\n\nMain Menu\n", default_color);
-		fn.cout("Option 1 - Number Gussing Game\nOption 2 - CannabisCalculator\nOption 3 - Quize\nOption 9 to Exit\n");
+		fn.cout("CoreFireCode 2024 edition\n", bright_green);
+		fn.cout("\n\nMain Menu\n", bright_white);
+		fn.cout("Option ", green); fn.cout("1 - Number Gussing Game\n", default_color);
+		fn.cout("Option ", green); fn.cout("2 - CannabisCalculator\n", default_color);
+		fn.cout("Option ", green); fn.cout("3 - Quiz\n", default_color);
+		fn.cout("Option ", green); fn.cout("9 - Exit\n", default_color);
 
 		switch (fn.selectMenuOption())
 		{
--- Corefire24/programExtensions.h
+++ Corefire24/programExtensions.h
@@ -44,11 +44,13 @@ public:
     virtual void clearScreen(); // Clear the console screen
     virtual void cout(const std::string& data); // Print text to the console
     virtual void cout(const std::string& data, const int set_text_color); // Print text to the console & set the text color
-    virtual void setScreenColor(const int backgroundColor, const int textColor); // Set the full screen color
+    //virtual void setScreenColor(const int backgroundColor, const int textColor); // Set the full screen color
+    virtual void setScreenColor(const char* screenTextColor); // Set the full screen color)
     virtual void set_text_color(const int data); // Set the text color in the console
     virtual void clearInputStream();
     virtual void extractInputStream();
-
+private:
+    bool isValidCommand(const char* command);
 };
 #endif // MY_CONSOLE_API_H
 
